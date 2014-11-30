e5216a4ed9633b34dcb544b06c1127a7ac4a9a82
Per Jansson <per.r.jansson@gmail.com>
2014-11-30 18:49:14 +0100

    Initial commit

---
+++ SimpleReminder/ReminderDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ReminderDetailViewController.swift
+//  SimpleReminder
+//
+//  Created by Per Jansson on 2014-11-30.
+//  Copyright (c) 2014 Per Jansson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ReminderDetailViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        automaticallyAdjustsScrollViewInsets = false
+    }
+    
+}
---
+++ SimpleReminder/ReminderListViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ReminderListViewController.swift
+//  SimpleReminder
+//
+//  Created by Per Jansson on 2014-11-30.
+//  Copyright (c) 2014 Per Jansson. All rights reserved.
+//
+
+import UIKit
+
+class ReminderListViewController: UITableViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var reminders = ["Buy milk", "Call Bob", "Walk the dog", "Push the code", "Make coffee", "Cut the lawn", "Paint the bedroom"];
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return reminders.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        cell.textLabel.text = self.reminders[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        self.performSegueWithIdentifier("showDetailSegue", sender: self)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        navigationController?.hidesBarsOnSwipe = true
+    }
+    
+    @IBAction func unwindToList(segue: UIStoryboardSegue) {
+        
+    }
+
+}
+
